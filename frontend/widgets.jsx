bb804c0f8eeefe18dd89fe0ce2e1c41fa5778ea5
tsai810417 <tsai_cheng@yahoo.com>
2018-06-07 22:45:47 -0700

    add new component

--- frontend/widgets.jsx
+++ frontend/widgets.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ClockWidget from './clock';
+import CalculatorWidet from './calculator'
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  ReactDOM.render(<ClockWidget />, root);
+  ReactDOM.render(
+    <div>
+      <ClockWidget />
+      <CalculatorWidet />
+    </div>
+    , root);
 })
