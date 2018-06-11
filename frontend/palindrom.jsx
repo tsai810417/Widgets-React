7f54a356da16eb91df63ca21c735783075a85a3d
tsai810417 <tsai_cheng@yahoo.com>
2018-06-11 16:36:56 -0700

    create main function to test if the string is palindrom

--- frontend/palindrom.jsx
+++ frontend/palindrom.jsx
@@ -7,6 +7,18 @@ class PalindromWiget extends React.Component {
       result: ''
     });
   }
+
+  palindrom(str) {
+    let l = str.length;
+    let i = 0;
+    while (i < l / 2) {
+      if (str[i] != str[l-i-1]) {
+        return false;
+      }
+      i += 1;
+    }
+    return true;
+  }
   }
 }
 
