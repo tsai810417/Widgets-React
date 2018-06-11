358a1530de98a5f7f0e7a6aabc596e66fd63e8a2
tsai810417 <tsai_cheng@yahoo.com>
2018-06-11 16:36:34 -0700

    create constructor

--- frontend/palindrom.jsx
+++ frontend/palindrom.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 class PalindromWiget extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = ({
+      str: '',
+      result: ''
+    });
+  }
+  }
 }
 
 export default PalindromWiget;
