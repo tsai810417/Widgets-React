12bd7b7efd32eb99c00b8ad12bcbbeb4bafd5485
tsai810417 <tsai_cheng@yahoo.com>
2018-06-14 10:15:20 -0700

    create AutocompleteWidget class

---
+++ frontend/autocomplete.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class AutocompleteWidget extends React.Componenet {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+
+  }
+}
+
+export default AutocompleteWidget;
