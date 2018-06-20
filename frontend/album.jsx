c377bddccc3b7998dc9ffaaba11168536855ce99
tsai810417 <tsai_cheng@yahoo.com>
2018-06-19 22:27:02 -0700

    create constructor function

--- frontend/album.jsx
+++ frontend/album.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 class AlbumWidget extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = ({
+      userInput: '',
+      albums: []
+    });
+  }
 }
 
 export default AlbumWidget;
