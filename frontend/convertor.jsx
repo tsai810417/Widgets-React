3a60376b6a71aa8d207d0880688f508fa48c18b7
tsai810417 <tsai_cheng@yahoo.com>
2018-06-21 21:36:38 -0700

    set the keys of the state

--- frontend/convertor.jsx
+++ frontend/convertor.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 class ConvertorWiget extends React.Component {
   constructor(props){
     super(props);
+    this.state = ({
+      amountFrom = '',
+      amountTo = '',
+      curFrom = '',
+      curTo = ''
+    });
   }
 
   render(){
